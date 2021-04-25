ac1d00668e97188420352abd269ad09e36182a92
Lavoro-app <83168586+Lavoro-app@users.noreply.github.com>
2021-04-25 15:59:15 +0300

    Add files via upload

---
+++ site/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>ArinMandarin</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Play&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Anonymous+Pro&display=swap" rel="stylesheet">
+  </head>
+  <body>
+
+    <header class="header">
+      <nav class="navigate">
+        <ul>
+          <li><a  class="nav_link_sun" id="toggleThemeBtn"><i class="material-icons" style="padding:10px;">brightness_7</i></a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <main class="main_beg" id="main">
+      <div class="left_main">
+      <h1 class="zag">Сервис "Evolution"</h1>
+      <h4 class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quidem delectus ullam nihil incidunt vitae harum accusantium illo asperiores tempore veritatis recusandae, ad distinctio omnis officia doloremque, commodi, quas impedit!</h4>
+      </div>
+      <div class="right_main">
+        <h1 class="zag">Каак же пользоваться</h1>
+      <h4 class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quidem delectus ullam nihil incidunt vitae harum accusantium illo asperiores tempore veritatis recusandae, ad distinctio omnis officia doloremque, commodi, quas impedit!</h4>
+      </div>
+      <a href="input/index.php" class="video">Далее</a>
+      </div>
+    </main>
+
+    <script type="text/javascript" src="dark.js"></script>
+    <script type="text/javascript">
+  //   function dark(){
+  //     body.style.background = "#fff";
+  //     sun.style.color = "#747679";
+  //     navb.style.color = "#747679";
+  //     navb1.style.color = "#747679";
+  //     navb2.style.color = "#747679";
+  //     main.style.background = "linear-gradient(86.42deg, rgb(196, 214, 0) 0%, rgb(91, 183, 132) 100%)";
+  //     main.style.color = "#fff";
+  //     document.getElementById('sun').onmouseover = function (){sun.style.color = "#87bb00";}
+  //     document.getElementById('sun').onmouseout = function () {sun.style.color = "#747679";}
+  //     document.getElementById('navb').onmouseover = function (){navb.style.color = "#87bb00";}
+  //     document.getElementById('navb').onmouseout = function () {navb.style.color = "#747679";}
+  //     document.getElementById('navb1').onmouseover = function (){navb1.style.color = "#87bb00";}
+  //     document.getElementById('navb1').onmouseout = function () {navb1.style.color = "#747679";}
+  //     document.getElementById('navb2').onmouseover = function (){navb2.style.color = "#87bb00";}
+  //     document.getElementById('navb2').onmouseout = function () {navb2.style.color = "#747679";}
+  //   }
+  //   function light(){
+  //   body.style.background = "#2f2f2f";
+  //   sun.style.color = "#fff";
+  //   navb.style.color = "#fff";
+  //   navb1.style.color = "#fff";
+  //   navb2.style.color = "#fff";
+  //   main.style.background = "linear-gradient(86.42deg, rgba(0, 60, 27, 1) 0%, rgba(38, 108, 0, 1) 100%)";
+  //   main.style.color = "white";
+  //   document.getElementById('sun').onmouseover = function (){sun.style.color = "#87bb00";}
+  //   document.getElementById('sun').onmouseout = function () {sun.style.color = "#fff";}
+  //   document.getElementById('navb').onmouseover = function (){navb.style.color = "#87bb00";}
+  //   document.getElementById('navb').onmouseout = function () {navb.style.color = "#fff";}
+  //   document.getElementById('navb1').onmouseover = function (){navb1.style.color = "#87bb00";}
+  //   document.getElementById('navb1').onmouseout = function () {navb1.style.color = "#fff";}
+  //   document.getElementById('navb2').onmouseover = function (){navb2.style.color = "#87bb00";}
+  //   document.getElementById('navb2').onmouseout = function () {navb2.style.color = "#fff";}
+  // }
+    // }
+
+          document.getElementById('button').onclick = function () {
+            form.style.display = "none";
+            thanks.style.display = "block";
+
+        }
+
+    </script>
+
+  </body>
+</html>
